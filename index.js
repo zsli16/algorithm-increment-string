d5bda27a4082a22e661fe0a399b0330903f44f0f
Sue Li <sue.li@post.harvard.edu>
2019-01-31 20:34:01 +0100

    solved except for edge case of zeroes..

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function incrementString (str) {
+  let numbers = str.match(/[0-9]/g);
+  if (numbers === null) return str + 1;
+  numbers = numbers.join('');
 
+  const numLength = numbers.length;
+
+  const posToreplace = str.length-numLength;
+
+  const newNum = parseInt(numbers) + 1;
+
+  str = str.split('');
+  console.log(str);
+  str.splice(posToreplace, numLength, newNum);
+  const res = str.join('');
+
+  return res;
 }
 
 module.exports = incrementString;
